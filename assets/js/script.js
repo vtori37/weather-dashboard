f159dc83af813dafe115fc3bf1cf53e39c6305c5
Tori Acer <vrice360@gmail.com>
2022-03-16 22:58:53 -0500

    refer. for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,9 @@ function searchBtn() {
   var city = ""
   var apiUrlKey= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid" + apiKey;
   fetch(apiUrlKey).then(function(data) {
-
+  
   });
+}
 
 
 // get weather
@@ -17,3 +18,10 @@ function searchBtn() {
 
 
 // uv color codes
+
+/* https://openweathermap.org/current
+https://openweathermap.org/forecast5
+https://openweathermap.org/api/geocoding-api#direct_name
+https://openweathermap.org/api/geocoding-api#direct  
+https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_api_fetch */
