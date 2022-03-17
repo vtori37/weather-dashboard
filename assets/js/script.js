f6488bb7c9b0868b04dcf6f8bec25cd84f770e97
Tori Acer <vrice360@gmail.com>
2022-03-16 22:55:30 -0500

    api url

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+console.log("what it doooo");
+
+function searchBtn() {
+  var city = ""
+  var apiUrlKey= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid" + apiKey;
+  fetch(apiUrlKey).then(function(data) {
+
+  });
+
+
+// get weather
+// varfetch
+
+// getcity location
+// varurl
+// fetch
+
+
+// uv color codes
