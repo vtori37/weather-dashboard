67131bba94d163f353f5c5a2e89b0274fec5fc76
Tori Acer <vrice360@gmail.com>
2022-03-19 23:30:58 -0500

    working weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 var getCity = document.getElementById("userCityInput");
 var apiKey = "&appid=f23b6b04e25cca30210187f4912be875&units=imperial";
 var baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var chosenCityEl= document.getElementById("chosenCity");
+var dateEL = document.getElementById("date");
+var tempEl= document.getElementById("temp");
+var tempLoHiEl= document.getElementById("tempLoHi");
+var windEl= document.getElementById("wind");
+var humidityEl= document.getElementById("humidity");
+var uvEl= document.getElementById("uvIndex");
+
+// Date and Time
+dateEL.textContent = moment().format("dddd, MMM, DD, YYYY, h:mm a");
 
 //listens to user clicking the button; sends user input to searchCity
 function searchBtn() {
@@ -22,23 +32,37 @@ function searchCity(city) {
     })
     .then(function(data){
       //  console.log(data);
-      //  console.log(data.coord);
+       console.log(data);
       
       var lon = data.coord.lon;
       var lat = data.coord.lat;
+
+      chosenCityEl.textContent = data.name;
+      tempEl.textContent = "Temperature: " + data.main.temp + "°F " + "Feels like: " + data.main.feels_like + "°";
+      tempLoHiEl.textContent = "High/Low: "  + data.main.temp_max + "°" + data.main.temp_min  + "°" ;
+      windEl.textContent = "Wind: " + data.wind.speed + "mph";
+      humidityEl.textContent = "Humidity: " + data.main.humidiy + "%";
+      uvEl.textContent = "UV Index:" + data.hourly.uvi + "nm";
+
+    }) 
+      .catch(function(error){
+        console.log(error);
+      })
+    };
+function fiveSearchCity(city) {    
+
        var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}${apiKey}` 
         fetch(url)
         .then(function(response){
           return response.json();
         })
         .then(function(data){
-          // console.log(data);
-          // console.log(data.list[0]);
+          var lonFive = data.coord.lon;
+          var latFive = data.coord.lat;
+          console.log(data.list[0]);
+          console.log(lat);
         })
 
-        console.log(lon);
-        console.log(lat);
-    })
     .catch(function(error){
         console.log(error);
       })
